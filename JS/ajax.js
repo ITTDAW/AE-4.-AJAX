3c6248e1a2b4ed9130f17a6344915cf3e102af8d
JMTE <JMTEdeveloper@outlook.es>
2021-11-19 16:08:07 +0100

    Se hacen los primeros cambios en AJAX

---
+++ JS/ajax.js
@@ -0,0 +1,269 @@
+//En una peticion asincrona el JS de nuestro navegador NO se queda bloqueado, por lo 
+//que tenemos que tener una funcion de callback que se ejecutará cuando el servidor
+//nos mande la respuesta
+
+function CargarPaginaConDatos(){
+    //El puerto que utiliza el live Server de Visual Code, recuerda que si lo tienes ocupado, live Server abre otro puerto y lo deberias de cambiar
+    const URL_DESTINO = "http://localhost:5500/"
+    const RECURSO = "pizzeria.json"
+    
+       
+      
+    //Siempre hay que formar el objeto XMLHttpRequest  
+            let xmlHttp = new XMLHttpRequest()
+            let procesar=document.querySelector("#procesar");
+            xmlHttp.onreadystatechange = function () { //Esta funcion se va a ejecutar CADA VEZ que haya un cambio en la propiedad
+                //"readyState"
+                 //Solo cuando la respuesta este completa y su estado sea 200 (OK) leeremos el mensaje
+        //del servidor y la procesamos
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        
+                        //Llamamos a nuestra funcion procesarRespuesta y le enviamos el objeto en String por parametro
+                        procesarRespuesta(this.responseText)
+                        
+
+                       
+                    }
+                     else {
+                        alert("ALGO VA MAL! ESTAS CONECTADO AL SERVIDOR?")
+                    }
+                }
+            }
+           // Para enviar una solicitud al servidor, se utiliza el método Open del objeto XMLHttpRequest () y send ():
+            xmlHttp.open('GET', URL_DESTINO + RECURSO, true)
+            xmlHttp.send(null)
+        
+    
+}
+
+
+function procesarRespuesta(jsonDoc) {
+    //Convertimos un String a un objeto JSON
+    var objetoJson = JSON.parse(jsonDoc)
+    
+    
+
+    
+    
+    
+    /**
+     * Ahora con nuestro objeto JSON, accedemos a los tamaños de la pizza y los introducimos en un array, tendremos un array de objetos
+     * ya que tamaños esta formado por TAMAÑO y PRECIO
+     */ 
+    let arrayTamaños=objetoJson.PIZZA.TAMAÑOS;
+    /**
+     * Accedemos a los ingredientes de la pizza y los introducimos en un array, tendremos un array de objetos
+     * ya que ingredientes esta formado por INGREDIENTES y PRECIO
+     */ 
+    let arrayIngredientes=objetoJson.PIZZA.INGREDIENTES;
+    
+    
+   
+   
+    /*A continuacion vamos a formar nuestros elementos del formulario que estan a la espera de recibir los datos del json. En este caso
+    estamos esperando a los valores de los tamaños y los ingredientes.
+    Para la parte de tamaños, tenemos que formar los radio button correspondientes e introducirlos en el fieldset existente.
+    Seleccionamos el elemento fieldset cuadrocheck y lo introducimos en una variable*/
+    let cuadrocheck=document.querySelector("#cuadrocheck");
+    
+    //Con un for recorremos nuestro arrayTamaños que hemos definido anteriormente
+    for (let i = 0; i < arrayTamaños.length; i++) {
+        //Para cada elemento del array formamos un div y le ponemos el nombre de clase para bootstrap
+        let div=document.createElement("div");
+        div.className="form-check";
+        //Para cada elemento creamos un input, le decimos que es de tipo radio , le introducimos el name tamaño para que formen todos parte del mismo bloque
+        //le introducimos el nombre de la clase correspondiente para formato bootstrap
+        let input=document.createElement("input");
+        input.type="radio";
+        input.name="tamaño";
+        input.className="form-check-input";
+        //creamos una etiqueta para cada radio
+        let label=document.createElement("label");
+        //Creamos una variable texto que contiene el nombre de cada tamaño segun su indice
+        let texto=arrayTamaños[i].TAMAÑO
+        //Le introducimos el nombre a la etiqueta la primera en mayuscula y lo restante en minuscula
+        label.textContent=texto[0].toUpperCase()+(texto.slice(1).toLowerCase());
+        //le establecemos el for a la etiqueta
+        label.htmlFor=texto;
+        //le establecemos el id al input
+        input.id=texto;
+        //agregamos al div creado al principio el input y el label
+        div.appendChild(input);
+        div.appendChild(label);
+        
+        //agregamos al fieldset el div
+        
+        cuadrocheck.appendChild(div);
+    
+    }
+
+    //La forma de crear los checkbox para los ingredientes es de una manera analoga al anterior caso pero con los checkbox y los ingredientes
+    let cuadroingredientes=document.querySelector("#cuadroingredientes");
+
+    for (let i = 0; i < arrayIngredientes.length; i++) {
+        let div=document.createElement("div");
+        div.className="form-check form-switch";
+        let input=document.createElement("input");
+        input.type="checkbox";
+        input.name="ingredientes";
+        input.className="form-check-input"
+        let label=document.createElement("label");
+        let texto=arrayIngredientes[i].INGREDIENTE;
+        label.textContent=texto[0].toUpperCase()+(texto.slice(1).toLowerCase());
+        label.htmlFor=texto;
+        input.id=texto;
+        div.appendChild(input);
+        div.appendChild(label);
+        
+        
+        
+        cuadroingredientes.appendChild(div);
+    
+    }
+    
+    
+}
+
+
+//Para traer los precios de los productos seleccionados por el cliente, hacemos una nueva peticion AJAX al servidor de la misma forma que 
+//la anterior
+function CargarPaginaParaPrecios(){
+    const URL_DESTINO = "http://localhost:5500/"
+    const RECURSO = "pizzeria.json"
+    
+       
+           
+    //Siempre hay que formar el objeto XMLHttpRequest
+            let xmlHttp = new XMLHttpRequest()
+            
+            xmlHttp.onreadystatechange = function () {
+                
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        
+                        //Esta va a ser la funcion que se ejecutará para calcular el valor de los productos escogidos por el cliente
+                        Respuesta(this.responseText)
+                        
+                        
+
+                       
+                    }
+                     else {
+                        alert("ALGO VA MAL! ESTAS CONECTADO AL SERVIDOR?")
+                    }
+                }
+            }
+             // Para enviar una solicitud al servidor, se utiliza el método Open del objeto XMLHttpRequest () y send ():
+            xmlHttp.open('GET', URL_DESTINO + RECURSO, true)
+            xmlHttp.send(null)
+        
+    
+}
+
+function Respuesta(jsonDoc) {
+    //Convertimos un texto a un objeto JSON
+    var objetoJson = JSON.parse(jsonDoc)
+   
+   //Creamos el array de objetos arrayTamaños y arrayIngredientes
+    let arrayTamaños=objetoJson.PIZZA.TAMAÑOS;
+    
+    let arrayIngredientes=objetoJson.PIZZA.INGREDIENTES;
+    
+
+    let i=0;
+    //Creamos una variable preciotamaño donde vamos a introducir el precio del tamaño escogido
+    let preciotamaño=0;
+    //Recorremos el grupo de los radio, buscando cual esta checked, como todos tienen el mismo name, solamente habrá uno seleccionado,
+    //cuando lo encuentra sale del bucle.
+    for( i=0;i<tamaño.length;i++){
+        
+        if (tamaño[i].checked==true){
+            //Establecemos el precio del tamaño escogido mediante buscando en nuestro array de objetos el objeto correspondiente al i y su precio establecido
+            preciotamaño=arrayTamaños[i].PRECIO;
+            break;
+        }
+    }
+    
+    //Ahora vamos a hacer lo mismo con los ingredientes
+    //Creamos una variable en donde almacenaremos el precio de los ingredientes total
+    let precioingredientes=0;
+    
+    //Creamos una variable en donde vamos a concatenar el texto ingrediente mas precio para la presentacion despues en el sweet alert
+    let cadenaIngredientesAñadidos="";
+
+        //Recorremos el array formado por los elementos ingredientes que tienen el mismo name, se podria recorrer tambien el arrayIngredientes
+         for(let j=0;j<ingredientes.length;j++){
+        //Cada vez que un ingrediente se encuentra seleccionado...
+        if(ingredientes[j].checked==true){
+            //Sumamos a nuestra variable el precio de ese ingrediente que lo sacamos segun el indice j que ocupa en ese array
+            precioingredientes=precioingredientes+arrayIngredientes[j].PRECIO
+           //Añadimos a nuestra cadena el nombre del ingrediente añadido y su precio
+            cadenaIngredientesAñadidos=cadenaIngredientesAñadidos.concat(arrayIngredientes[j].INGREDIENTE+
+            " "+arrayIngredientes[j].PRECIO + " euros"+" // ");
+        }
+        }
+        
+
+        //Ahora solo nos queda presentar en nuestro mensaje final los datos que hemos conseguido
+        swal({
+            title:"Has pedido una pizza \n"
+             + arrayTamaños[i].TAMAÑO.toUpperCase() + " su precio es de " + arrayTamaños[i].PRECIO + " euros",
+            text: "Has añadido los siguientes ingredientes:  \n" +cadenaIngredientesAñadidos,
+            
+            type: 'info',
+           
+            confirmButtonColor: '#3085d6',
+           
+            confirmButtonText: 'OK'
+          }).then((result) => {
+            
+                swal(
+    
+                    {
+                        title              : "El precio total de la pizza son: \n" + 
+                        "TAMAÑO: "+ preciotamaño+ " euros\n" +
+                        "INGREDIENTES: " + precioingredientes + " euros\n"+
+                        "TOTAL: " +(preciotamaño+precioingredientes) + " euros",
+                        text               : "¿Deseas hacer el pedido?",
+                        type               : "success",
+                        allowEscapeKey     : false,
+                        allowOutsideClick  : false,
+                        showCancelButton   : true,
+                        confirmButtonColor : "#DD6B55",
+                        confirmButtonText  : "Yes",
+                        showLoaderOnConfirm: true,
+                        closeOnConfirm     : false,
+                        //en sweet alert para el submit es necesario el parametro preconfirm, sino no envia formulario, ya que tiene que ser async
+                        preConfirm:   function (isConfirm) {
+                        
+                            if (isConfirm) {
+                                //Aqui es donde hacemos el submit del formulario, cuando ya tenemos todo preparado
+                                document.formulario.submit();
+                                
+                                return true;
+                            }else{
+                                return false;
+                            }
+                    
+                            
+                    
+                        }
+                    }
+                
+                   
+                )    
+    
+    
+       
+    
+            
+          })
+}
+
+
+
+
+
+
+   
---
+++ JS/validaciones.js
@@ -0,0 +1,203 @@
+
+/*El evento onload, se dispara al final del proceso de carga del documento. Es decir, cuando todos los objetos del DOM 
+(imágenes, flash, scripts, frames) han terminado de cargarse. Una excepción son las hojas de estilo, que no siempre están 
+cargadas al momento de lanzarse este evento. */
+
+window.onload= function(){
+    //Cuando pulsemos el input button con id=procesar del formulario , activamos la funcion de validacion que tenemos descrita debajo.
+    //Si todos en todos los campos que validamos no obtenemos un ningun false, la validacion sera correcta. Si obtenemos algun
+    //false, mostraremos un alert y no podremos enviar los datos del formulario
+    //Hemos utilizado un boton y no un submit para poder utilizar los sweet alert. De esta manera, el submit se realiza
+    //cuando pulsamos el boton ok en el ultimo sweet alert de nuestro pedido.
+    procesar.onclick =validacion;
+   
+    /*Al mismo tiempo, vamos a realizar una petición AJAX al servidor para presentar los datos correspondientes a 
+    tamaño e ingredientes de la pizza, para ello ejecutamos la siguiente función que se encuentra en el archivo ajax.js */
+    CargarPaginaConDatos();
+   
+
+   
+}
+
+function validacion(){
+   
+
+    //AQUI VALIDAMOS QUE TODOS LOS CAMPOS TIENEN QUE ESTAR CUBIERTOS
+    /*Con el  método trim( ) nos devuelve la cadena de texto despojada de los espacios en blanco en ambos extremos. 
+    Además el método no afecta al valor de la cadena de texto. Por lo tanto si el valor de los campos que tenemos definidos
+    a continuacion quitando los espacios en blanco, estan vacios mostraremos el alert y devolveremos el false en la funcion */
+
+
+   if (nombre.value.trim() == "" || telefono.value.trim()=="" || direccion.value.trim()=="" || email.value.trim()=="") {
+        // Si no se cumple la condicion...
+        swal({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Los campos Nombre, Direccion, Telefono y Email deben de estar cubiertos',
+            footer: '<a href="">Rellena todos los campos para hacer un pedido</a>'
+          })
+       
+        return false;
+    }
+
+
+     //AQUI IMPLEMENTAMOS CON EXPRESIONES REGULARES QUE LA PRIMERA LETRA DEL NOMBRE EMPIECE POR MAYUSCULAS
+     /*Definimos nuestra variable primeramayuscula de tal forma que nuestra palabra tiene que empezar por una letra(^) del rango que 
+     establecemos [A-Z]*/
+     let primeramayuscula = /^[A-Z]/
+    
+     /*Con el metodo match nos devuelve todas las ocurrencias que haya dentro de una cadena teniendo en cuenta una expresión regular 
+     establecida. Nosotros mediante el condicional if, le decimos que si no existe la ocurrencia de que nuestra palabra empiece por
+     mayuscula, enviamos un alert y devolvemos false en la funcion*/
+    
+    
+    
+     if (!nombre.value.match(primeramayuscula)){
+        swal({
+            type: 'error',
+            title: 'Oops...',
+            text: 'El nombre debe de tener la primera letra Mayúscula',
+            footer: '<a href="">Escribe la primera letra mayúscula en el nombre</a>'
+          })
+          return false;
+      }
+
+      //AQUI IMPLEMENTAMOS QUE EL TELEFONO SEA NUMERICO Y SOLO TENGA 9 CARACTERES
+
+      /*De la misma forma que antes, definimos la expresion regular que queremos y la introducimos en nuestra variable
+      En este caso le decimos que tiene que empezar por un caracter comprendido entre [0-9] y que la longitud debe de ser de 9 numeros exactos */
+    
+    
+    
+      let tel=/^[0-9]{9,9}$/;
+      /*El metodo match nos devuele las ocurrencias que haya dentro de la cadena si cumple con nuestra expresion regular, con nuestro 
+      condicional if, comprobamos si es falsa esta ocurrencia, pues devolvemos el alert correspondiente y retornamos false en la funcion */
+    
+    
+      if (!telefono.value.match(tel)){
+        swal({
+            type: 'error',
+            title: 'Oops...',
+            text: 'El teléfono puede ser movil o fijo, pero ha de tener 9 digitos',
+            footer: '<a href="">9 digitos en tu telefono</a>'
+          })
+         return false;
+     }
+
+     //AQUI IMPLEMENTAMOS QUE EL CORREO SEA UN CORREO VALIDO
+
+     /*Definimos una expresión regular para una implementación válida para la mayoria de correos
+     Empieza con cualquier caracter menos la @ despues debe de llevar una @ seguido de caracteres excepto @ y cuando lleve el punto, puede llevar
+    caracteres letras minimo 2*/
+    let correo=/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/
+
+    /*Volvemos a analizar con el condicional if y el metodo match si se produce la ocurrencia de nuestra expresion regular */
+    
+    
+    if (!email.value.match(correo)){
+        //Si no se produce devolvemos el alert y el false 
+        swal({
+            type: 'error',
+            title: 'Oops...',
+            text: 'El campo email debe de tener un formato correcto',
+            footer: '<a href="">Pon un correo electronico correcto</a>'
+          })
+       return false;
+    }
+
+
+
+    
+
+    //AQUI VALIDAMOS QUE ESCOGEMOS AL MENOS UN TAMAÑO DE LA PIZZA
+
+    //Seleccionamos nuestro grupo de input radio mediante el name que le definimos en el html y lo introducimos en la variable tamaño
+    tamaño=document.getElementsByName("tamaño");
+    //Definimos una variable llamada check y la ponemos a false
+    let check=false;
+    //Definimos la variable ipara el for que despues la utilizaremos para nuestro switch, por esa razon tiene que definirse aqui
+    let i;
+    //Ahora mediante un bucle for, recorremos el tamaño del array que forma nuestra variable tamaño con los elementos agrupados radio por name
+    for(i=0;i<tamaño.length;i++){
+        //Cuando el elemento correspondiente se encuentre checked ponemos la variable check a true y salimos del bucle
+        if (tamaño[i].checked==true){
+            check=true;
+            break;
+        }
+    }
+    //Si hemos salido del bucle y la variable check no esta a true, devolvemos un alert y enviamos un false.
+    if(check==false){
+        swal({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Debes de seleccionar un tamaño de la pizza',
+            footer: '<a href="">Escoge un tamaño de pizza</a>'
+          })
+        return false;
+    
+    }
+
+ 
+    
+    //AQUI VALIDAMOS QUE ALGUN INGREDIENTE SE ENCUENTRE SELECCIONADO
+    //Con checked obligamos a que algun ingrediente se encuentre seleccionado
+    ingredientes=document.getElementsByName("ingredientes");
+    let check2=false;
+    for(i=0;i<ingredientes.length;i++){
+        //Cuando el elemento correspondiente se encuentre checked ponemos la variable check a true y salimos del bucle
+        if (ingredientes[i].checked==true){
+            check2=true;
+            break;
+        }
+    }
+    if(!check2){
+        //Si no hay ninguno seleccionado, devolvemos un alert y un false.
+        swal({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Selecciona al menos un ingrediente en la pizza',
+            footer: '<a href="">Escoge al menos un ingrediente</a>'
+          })
+        return false;
+
+    }
+
+    CargarPaginaParaPrecios();
+
+
+  
+
+    //SI TODAS LAS VALIDACIONES NO DEVUELVEN NINGUN FALSE, PODEMOS CALCULAR EL PRECIO DE NUESTRA PIZZA
+
+    //Primero mostramos un alert con el tamaño de la pizza pedida y el precio que supone ese tamaño, tenemos los valores en las variables que hemos guardado
+    // swal("La pizza pedida es: \n"
+    // + "Tamaño : "+ tam.toUpperCase() + " " + preciotamaño + " euros" );
+
+    /*Despues para saber cuantos ingredientes hemos seleccionado, seleccionamos nuestro grupo checkbox con el name que le establecimos
+    en el html y lo recorremos con un bucle for */
+    
+   
+    
+    
+
+    //Con el siguiente alert mostramos el numero total de ingredientes que hemos seleccionado y el precio de los mismos
+    // swal("Has añadido :\n"+ 
+    // precioingredientes + " ingredientes: " + precioingredientes + "euros.")
+
+    //Y en el ultimo sweet alert devolvemos el valor total de la pizza desglosando sus apartados
+   
+   
+      
+    
+   
+     
+
+
+     
+            
+}
+
+
+
+
+
